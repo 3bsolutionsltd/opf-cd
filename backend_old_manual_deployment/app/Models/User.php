c488a816a1a210d4e008ba0f38a6aad57fdcb4eb
Stephen Omwony <3bsolutionsltd@gmail.com>
2026-02-17 19:49:26 +0300

    Fix User model to use password_hash field matching PostgreSQL schema

--- backend_old_manual_deployment/app/Models/User.php
+++ backend_old_manual_deployment/app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
         'email',
-        'password',
+        'password_hash',
+        'role',
+        'is_active',
     ];
 
     /**
@@ -29,7 +30,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $hidden = [
-        'password',
+        'password_hash',
         'remember_token',
     ];
 
@@ -42,7 +43,18 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password_hash' => 'hashed',
+            'is_active' => 'boolean',
         ];
+    }
+
+    /**
+     * Get the password for authentication.
+     * 
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password_hash;
     }
 }
